ca836872068c8bbf6be23b990251e3f3ca495b08
Zippy <zippyy@zoho.com>
2026-07-15 07:33:36 -0600

    Keep purchase verification scoped

--- app-shared/Sources/CommonLibraryCore/IAP/IAPManager+Verify.swift
+++ app-shared/Sources/CommonLibraryCore/IAP/IAPManager+Verify.swift
@@ -6,9 +6,6 @@ import Partout
 
 extension IAPManager {
     public func verify(_ profile: Profile, extra: Set<ABI.AppFeature>? = nil) throws {
-        guard isEnabled else {
-            return
-        }
         var features = profile.features
         extra?.forEach {
             features.insert($0)
@@ -17,9 +14,6 @@ extension IAPManager {
     }
 
     public func verify(_ features: Set<ABI.AppFeature>) throws {
-        guard isEnabled else {
-            return
-        }
 #if os(tvOS)
         guard isEligible(for: .appleTV) else {
             throw ABI.AppError.ineligibleProfile(features.union([.appleTV]))
